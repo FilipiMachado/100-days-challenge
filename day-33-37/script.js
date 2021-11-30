72c48f1b98f768b8926e546898b162989727044e
Filipi <filipisee@hotmail.com>
2021-11-30 10:56:47 -0300

    updated scripts

--- day-33-37/script.js
+++ day-33-37/script.js
@@ -1,8 +1,15 @@
 let inputElement = document.querySelector(".input-text");
-let remainingChars = document.querySelector(".remaining-chars");
+let remainingCharsEl = document.querySelector(".remaining-chars");
+
+// console.dir(inputElement);
+
+let allowedChars = inputElement.maxLength;
 
 const updateChars = (e) => {
   let enteredText = e.target.value;
+  let enteredTextLength = enteredText.length;
+  let remainingChars = 60 - enteredTextLength;
+
 };
 
 inputElement.addEventListener("input", updateChars);
