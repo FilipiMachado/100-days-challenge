0d96e5c22df0eeaae32ec258af517885bef4bd75
Filipi <filipisee@hotmail.com>
2021-12-01 10:31:33 -0300

    conditional tests

--- day-37-39/conditional-code.js
+++ day-37-39/conditional-code.js
@@ -1,4 +1,4 @@
-let inputElement = document.querySelector(".input-text");
+/* let inputElement = document.querySelector(".input-text");
 let remainingCharsEl = document.querySelector(".remaining-chars");
 
 // console.dir(inputElement);
@@ -9,14 +9,25 @@ const updateChars = (e) => {
   let enteredText = e.target.value;
   let enteredTextLength = enteredText.length;
   let remainingChars = allowedChars - enteredTextLength;
-  
+
   remainingCharsEl.textContent = remainingChars;
 };
-
-
 
 inputElement.addEventListener("input", updateChars);
 
 if (remainingChars <= 10) {
-    remainingCharsEl.classList.add("error")
+  remainingCharsEl.classList.add("error");
+} */
+
+const loggedUser = {
+  name: "Fil",
+  age: 25,
+  isAdmin: true,
+};
+
+for (const propertyName in loggedUser) {
+  console.log(propertyName);
+  console.log(loggedUser[propertyName]);
 }
+
+
