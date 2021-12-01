fbb15d09d5f59d9f5fe07dd48ff43f2732396012
Filipi <filipisee@hotmail.com>
2021-12-01 09:51:33 -0300

    conditional tests

---
+++ day-37-39/conditional-code.js
@@ -0,0 +1,22 @@
+let inputElement = document.querySelector(".input-text");
+let remainingCharsEl = document.querySelector(".remaining-chars");
+
+// console.dir(inputElement);
+
+let allowedChars = inputElement.maxLength;
+
+const updateChars = (e) => {
+  let enteredText = e.target.value;
+  let enteredTextLength = enteredText.length;
+  let remainingChars = allowedChars - enteredTextLength;
+  
+  remainingCharsEl.textContent = remainingChars;
+};
+
+
+
+inputElement.addEventListener("input", updateChars);
+
+if (remainingChars <= 10) {
+    remainingCharsEl.classList.add("error")
+}
